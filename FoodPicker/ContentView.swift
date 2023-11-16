ae234aa1f6db20233aadace5ab468ec2461c7064
liyoway <liyoway@gmail.com>
2023-11-16 23:38:41 +0800

    修改佈局

--- FoodPicker/ContentView.swift
+++ FoodPicker/ContentView.swift
@@ -7,115 +7,144 @@
 
 import SwiftUI
 
+// ContentView結構定義了用戶界面的佈局和行為
 struct ContentView: View {
+    // 聲明一些狀態和屬性
     let food = Food.examples
     @State private var selectedFood: Food?
-
+    @State private var showInfo: Bool = false
+    
+    // body定義了視圖的內容
     var body: some View {
-        VStack(spacing: 30.0) {
-            Group {
-                if selectedFood != .none {
-                    Text(selectedFood!.image)
-                        .font(.system(size: 200))
-                        .minimumScaleFactor(0.1)
-                        .lineLimit(1)
-                } else {
-                    Image("dinner")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
+        ScrollView {
+            VStack(spacing: 30.0) {
+                // 根據是否有選擇的食物來決定顯示的內容
+                Group {
+                    if selectedFood != nil {
+                        Text(selectedFood!.image)
+                            .font(.system(size: 200))
+                            .minimumScaleFactor(0.1)
+                            .lineLimit(1)
+                    } else {
+                        Image("dinner")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
                 }
-            }
-            .frame(height: 250)
-            .border(.red)
-
-            Text("今天吃什麼？")
-                .bold()
-
-            if selectedFood != .none {
-                let calorie = Int(selectedFood!.calorie)
-                let protein = Int(selectedFood!.protein)
-                let fat = Int(selectedFood!.fat)
-                let carb = Int(selectedFood!.carb)
+                .frame(height: 250)
                 
-                HStack {
-                    Text("\(selectedFood!.name)")
-                        .foregroundStyle(.orange)
-                        .bold()
-                        .font(.largeTitle)
-                    Image(systemName: "info.circle.fill")
-                        .foregroundStyle(.secondary)
+                Text("今天吃什麼？")
+                    .bold()
+                
+                // 如果選擇了食物，顯示詳細資訊
+                if selectedFood != nil {
+                    let calorie = Int(selectedFood!.calorie)
+                    
+                    HStack {
+                        Text("\(selectedFood!.name)")
+                            .foregroundStyle(.orange)
+                            .bold()
+                            .font(.largeTitle)
+                            .id(selectedFood!.name)
+                            .transition(.asymmetric(
+                                insertion: .opacity
+                                    .animation(.easeInOut(duration: 0.5).delay(0.2)),
+                                removal: .opacity
+                                    .animation(.easeInOut(duration: 0.4))))
+                        Button {
+                            showInfo.toggle()
+                        } label : {
+                            Image(systemName: "info.circle.fill")
+                                .foregroundStyle(.secondary)
+                        }.buttonStyle(.plain)
+                    }
+
+                    Text("熱量\(calorie) 大卡")
+                        .font(.title2)
                 }
-                .id(selectedFood!.name)
-                .transition(.asymmetric(
-                    insertion: .opacity
-                        .animation(.easeInOut(duration: 0.5).delay(0.2)),
-                    removal: .opacity
-                        .animation(.easeInOut(duration: 0.4))))
-                Text("熱量 \(calorie) 大卡")
-                    .font(.title2)
-                HStack {
-                    VStack(spacing: 12) {
-                        Text("蛋白質")
-                        Text("\(protein)g")
-                    }
-                    Divider().padding(.horizontal)
-                    VStack(spacing: 12) {
-                        Text("脂肪")
-                        Text("\(fat)g")
-                    }
-                    Divider().padding(.horizontal)
+                
+                // 顯示營養資訊的區塊
+                VStack {
+                    if showInfo {
+                        Grid(horizontalSpacing: 12, verticalSpacing: 12) {
+                            let calorie = Int(selectedFood!.calorie)
+                            let protein = Int(selectedFood!.protein)
+                            let fat = Int(selectedFood!.fat)
+                            let carb = Int(selectedFood!.carb)
 
-                    VStack(spacing: 12) {
-                        Text("碳水")
-                        Text("\(carb)g")
+                            GridRow {
+                                Text("蛋白質")
+                                Text("脂肪")
+                                Text("碳水")
+                            }.frame(minWidth: 60)
+                            
+                            Divider()
+                                .gridCellUnsizedAxes(.horizontal)
+                                .padding(.horizontal, -10)
+                            
+                            GridRow {
+                                Text("\(protein)g")
+                                Text("\(fat)g")
+                                Text("\(carb)g")
+                            }
+                        }
+                        .font(.title3)
+                        .padding(.horizontal)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 8).foregroundColor(Color(.systemBackground)))
+                        .transition(.move(edge: .top).combined(with: .opacity))
                     }
                 }
-                .font(.title2)
-                .padding(.horizontal)
-                .background(RoundedRectangle(cornerRadius: 8).foregroundColor(Color(.systemBackground)))
+                .clipped()
+                .frame(maxWidth: .infinity)
+                
+                // 其他按鈕和布局
+                Spacer().layoutPriority(0)
+                
+                Button(role: .none) {
+                    selectedFood = food.randomElement()
+                } label: {
+                    Text(selectedFood == nil ? "告訴我！" : "換一個")
+                        .multilineTextAlignment(.center)
+                        .frame(width: 200.0)
+                }
+                .padding(.bottom, -15)
+                
+                Button(role: .none) {
+                    selectedFood = nil
+                    showInfo = false
+                } label: {
+                    Text("重置")
+                        .multilineTextAlignment(.center)
+                        .frame(width: 200.0)
+                }
+                .buttonStyle(.bordered)
             }
-
-            
-            Spacer()
-
-            Button(role: .none) {
-                selectedFood = food.randomElement()
-            } label: {
-                Text(selectedFood == .none ? "告訴我！" : "換一個")
-                    .multilineTextAlignment(.center)
-                    .frame(width: 200.0)
-            }
-            .padding(.bottom, -15)
-
-            Button(role: .none) {
-                selectedFood = .none
-            } label: {
-                Text("重置")
-                    .multilineTextAlignment(.center)
-                    .frame(width: 200.0)
-            }
-            .buttonStyle(.bordered)
+            .padding()
+            .frame(maxWidth: .infinity, minHeight: UIScreen.main.bounds.height - 100)
+            .font(.largeTitle)
+            .buttonStyle(.borderedProminent)
+            .animation(.spring(dampingFraction: 0.55), value: showInfo)
+            .animation(.easeInOut, value: selectedFood)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(.secondarySystemBackground))
-        .font(.largeTitle)
-        .buttonStyle(.borderedProminent)
-        .animation(.easeInOut, value: selectedFood)
-        .buttonBorderShape(.capsule)
-        .controlSize(.large)
     }
 }
 
+// 擴展ContentView以便於預覽
 extension ContentView {
+    // 自定義初始化方法，方便預覽時指定食物
     init(selectedFood: Food) {
         _selectedFood = State(wrappedValue: selectedFood)
     }
 }
 
-// 預覽部分
+// 提供ContentView的預覽
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(selectedFood: .examples.first!)
     }
 }
+
