3757e6403a1212e02984f5012cd98b2fc5b9d534
liyoway <liyoway@gmail.com>
2023-11-16 14:47:25 +0800

    修改UI

--- FoodPicker/ContentView.swift
+++ FoodPicker/ContentView.swift
@@ -8,28 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    let food = Food.examples
+    @State private var selectedFood: Food?
 
-    let food = ["漢堡", "沙拉", "披薩", "義大利麵", "雞腿便當", "刀削麵", "火鍋", "牛肉麵", "關東煮"]
-    
-    @State private var selectedFood: String?
-    
     var body: some View {
         VStack(spacing: 30.0) {
-            Image("dinner")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            
-            Text("今天吃什麼？")
-                .font(.title)
-                .bold()
-            
-            if selectedFood != .none {
-                Text(selectedFood ?? "")
-                    .font(.largeTitle)
-                    .bold()
-                    .foregroundStyle(.orange)
+            Group {
+                if selectedFood != .none {
+                    Text(selectedFood!.image)
+                        .font(.system(size: 200))
+                        .minimumScaleFactor(0.1)
+                        .lineLimit(1)
+                } else {
+                    Image("dinner")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
             }
+            .frame(height: 250)
+            .border(.red)
+
+            Text("今天吃什麼？")
+                .bold()
+
+            if selectedFood != .none {
+                let calorie = Int(selectedFood!.calorie)
+                let protein = Int(selectedFood!.protein)
+                let fat = Int(selectedFood!.fat)
+                let carb = Int(selectedFood!.carb)
+                
+                HStack {
+                    Text("\(selectedFood!.name)")
+                        .foregroundStyle(.orange)
+                        .bold()
+                        .font(.largeTitle)
+                    Image(systemName: "info.circle.fill")
+                        .foregroundStyle(.secondary)
+                }
+                .id(selectedFood!.name)
+                .transition(.asymmetric(
+                    insertion: .opacity
+                        .animation(.easeInOut(duration: 0.5).delay(0.2)),
+                    removal: .opacity
+                        .animation(.easeInOut(duration: 0.4))))
+                Text("熱量 \(calorie) 大卡")
+                    .font(.title2)
+                HStack {
+                    VStack(spacing: 12) {
+                        Text("蛋白質")
+                        Text("\(protein)g")
+                    }
+                    Divider().padding(.horizontal)
+                    VStack(spacing: 12) {
+                        Text("脂肪")
+                        Text("\(fat)g")
+                    }
+                    Divider().padding(.horizontal)
+
+                    VStack(spacing: 12) {
+                        Text("碳水")
+                        Text("\(carb)g")
+                    }
+                }
+                .font(.title2)
+                .padding(.horizontal)
+                .background(RoundedRectangle(cornerRadius: 8).foregroundColor(Color(.systemBackground)))
+            }
+
             
+            Spacer()
+
             Button(role: .none) {
                 selectedFood = food.randomElement()
             } label: {
@@ -38,8 +86,7 @@ struct ContentView: View {
                     .frame(width: 200.0)
             }
             .padding(.bottom, -15)
-            
-            
+
             Button(role: .none) {
                 selectedFood = .none
             } label: {
@@ -50,7 +97,7 @@ struct ContentView: View {
             .buttonStyle(.bordered)
         }
         .padding()
-        .frame(maxHeight: .infinity)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(.secondarySystemBackground))
         .font(.largeTitle)
         .buttonStyle(.borderedProminent)
@@ -60,6 +107,15 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
+extension ContentView {
+    init(selectedFood: Food) {
+        _selectedFood = State(wrappedValue: selectedFood)
+    }
+}
+
+// 預覽部分
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(selectedFood: .examples.first!)
+    }
 }
