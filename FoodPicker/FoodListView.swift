38df1e6e4037ef93f31ff2eac4260cc07347718d
liyoway <liyoway@gmail.com>
2024-01-17 23:38:48 +0800

    update sheet

--- FoodPicker/FoodListView.swift
+++ FoodPicker/FoodListView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct FoodListView: View {
     @Environment(\.editMode) var editMode
+    @Environment(\.dynamicTypeSize) var textSize
     @State private var food = Food.examples
     @State private var selectedFood = Set<Food.ID>()
     
@@ -19,7 +20,7 @@ struct FoodListView: View {
             titleBar
             
             List($food, editActions: .all, selection: $selectedFood) { $food in
-                Text(food.name)
+                Text(food.name).padding(.vertical, 10)
            }
             .listStyle(.plain)
             .padding(.horizontal)
@@ -27,9 +28,27 @@ struct FoodListView: View {
         }
         .background(.groupBG)
         .safeAreaInset(edge: .bottom, content: buildFloatButton)
+        .sheet(isPresented: .constant(true)) {
+            let food = food.first!
+            let shouldUseVstack = textSize.isAccessibilitySize || food.image.count > 1
+            
+            AnyLayout.useVStack(if: shouldUseVstack, spacing: 30) {
+                Text(food.image)
+                    .font(.system(size: 100))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+                Grid(horizontalSpacing: 30, verticalSpacing: 12) {
+                    buildNutrionView(title: "熱量", value: food.$calorie)
+                    buildNutrionView(title: "蛋白質", value: food.$protein)
+                    buildNutrionView(title: "脂肪", value: food.$fat)
+                    buildNutrionView(title: "碳水", value: food.$carb)
+                }.presentationDetents([.medium])
+                    .padding()
+            }
         }
     }
-
+        
+}
 
 private extension FoodListView {
     
@@ -39,7 +58,7 @@ private extension FoodListView {
                 .font(.title.bold())
                 .foregroundColor(.accentColor)
                 .frame(maxWidth: .infinity, alignment: .leading)
-
+            
             EditButton()
                 .buttonStyle(.bordered)
         } .padding()
@@ -53,7 +72,7 @@ private extension FoodListView {
                 .symbolRenderingMode(.palette)
                 .foregroundStyle(.white, Color.accentColor.gradient)
         }
-
+        
     }
     
     var removeButton : some View {
@@ -72,20 +91,28 @@ private extension FoodListView {
     
     func buildFloatButton() -> some View {
         ZStack {
-                removeButton
+            removeButton
                 .transition(.move(edge: .leading).combined(with: .opacity).animation(.easeInOut))
                 .opacity(isEditing ? 1 : 0)
                 .id(isEditing)
             
-                HStack {
-                    Spacer()
-                    addButton
-                        .scaleEffect(isEditing ? 0 : 1)
-                        .opacity(isEditing ? 0 : 1)
-                        .animation(.easeInOut, value: isEditing)
-                }
+            HStack {
+                Spacer()
+                addButton
+                    .scaleEffect(isEditing ? 0 : 1)
+                    .opacity(isEditing ? 0 : 1)
+                    .animation(.easeInOut, value: isEditing)
             }
         }
+    }
+    
+    func buildNutrionView(title: String, value: String) -> some View {
+        GridRow {
+            Text(title).gridCellAnchor(.leading)
+            Text(value).gridCellAnchor(.trailing)
+            
+        }
+    }
 }
 
 #Preview {
